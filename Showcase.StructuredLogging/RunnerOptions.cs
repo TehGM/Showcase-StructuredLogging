e64a07411ee7c97dcb7f96c51f150d29b84e049d
TehGM <wojop95@gmail.com>
2021-11-05 12:09:41 +0000

    Slight increase to runner start delay

--- Showcase.StructuredLogging/RunnerOptions.cs
+++ Showcase.StructuredLogging/RunnerOptions.cs
@@ -5,7 +5,7 @@ namespace TehGM.Showcase.StructuredLogging
     public class RunnerOptions
     {
         /// <summary>How long runner will wait before starting.</summary>
-        public TimeSpan StartDelay { get; set; } = TimeSpan.FromMilliseconds(50);
+        public TimeSpan StartDelay { get; set; } = TimeSpan.FromMilliseconds(100);
         /// <summary>Delay after which the task will be cancelled.</summary>
         public TimeSpan CancellationDelay { get; set; } = TimeSpan.FromSeconds(5);
         /// <summary>How often the runner should log a tick.</summary>
