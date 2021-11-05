423304926d07477da5011e7ff1d8744101a2704b
TehGM <wojop95@gmail.com>
2021-11-05 10:56:44 +0000

    Add project files.

---
+++ Showcase.StructuredLogging/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace TehGM.Showcase.StructuredLogging
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            // create a generic host
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // load secrets files if present
+                    config.AddJsonFile("appsecrets.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile($"appsecrets.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
+                .UseSerilog((context, config)
+                    => config.ReadFrom.Configuration(context.Configuration, "Logging"),
+                    preserveStaticLogger: true)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<Runner>();
+                })
+                .Build();
+            return host.RunAsync();
+        }
+    }
+}
---
+++ Showcase.StructuredLogging/Runner.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TehGM.Showcase.StructuredLogging
+{
+    class Runner : IHostedService
+    {
+        private readonly ILogger _log;
+
+        public Runner(ILogger<Runner> log)
+        {
+            this._log = log;
+        }
+
+        async Task IHostedService.StartAsync(CancellationToken cancellationToken)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            try
+            {
+                // log messages here
+            }
+            catch (OperationCanceledException ex)
+            {
+                // log exception here
+            }
+        }
+
+        Task IHostedService.StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
